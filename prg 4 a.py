7bba9e5f293ec2c55d8f3eddd459759c0b181449
sarveshp22 <131164672+sarveshp22@users.noreply.github.com>
2023-07-09 23:18:01 +0530

    Add files via upload

---
+++ prg 4 a.py
@@ -0,0 +1,10 @@
+def reverse_tuple(t):
+    
+    reversed_tuple = tuple(reversed(t))
+    return reversed_tuple
+
+t = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h')
+
+result = reverse_tuple(t)
+
+print(result)
