2eccaa0093d7569d193614b1e6f4ce83eed0ed17
sarveshp22 <131164672+sarveshp22@users.noreply.github.com>
2023-07-09 23:19:04 +0530

    Add files via upload

---
+++ prg 6.py
@@ -0,0 +1,48 @@
+
+Grocery = {
+    "Cornflakes": {"quantity": 15, "price": 100},
+    "Muesli": {"quantity": 14, "price": 150},
+    "Oats": {"quantity": 10, "price": 80},
+    "Wheat Flakes": {"quantity": 5, "price": 75},
+    "Granola": {"quantity": 8, "price": 125}
+}
+
+print("Item details before updating:\n")
+print(Grocery)
+
+print("\nWhat do you want to do?")
+print("1. Add additional quantity of the cereals")
+print("2. Update the price of the grocery")
+print("3. Add new item")
+choice = int(input("Enter your choice: "))
+
+if choice == 1:
+    item_name = input("Enter the grocery item name: ")
+    quantity = int(input("Enter the quantity of item to be added: "))
+    if item_name in Grocery:
+        Grocery[item_name]["quantity"] += quantity
+        print("\nItem details after updating:\n")
+        print(Grocery)
+    else:
+        print("Item does not exist in the grocery list.")
+
+elif choice == 2:
+    item_name = input("Enter the grocery item name: ")
+    price = float(input("Enter the new price of the item: "))
+    if item_name in Grocery:
+        Grocery[item_name]["price"] = price
+        print("\nItem details after updating:\n")
+        print(Grocery)
+    else:
+        print("Item does not exist in the grocery list.")
+        
+elif choice == 3:
+    item_name = input("Enter the new grocery item name: ")
+    quantity = int(input("Enter the quantity of the new item: "))
+    price = float(input("Enter the price of the new item: "))
+    Grocery[item_name] = {"quantity": quantity, "price": price}
+    print("\nItem details after updating:\n")
+    print(Grocery)
+
+else:
+    print("Invalid choice.")
